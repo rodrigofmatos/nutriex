ddf9d90f6511d19c3391147d6085c77123584145
Rodrigo Fernandes Matos <42415874+rodrigofmatos@users.noreply.github.com>
2021-06-27 22:46:48 -0300

    Delete rpa001_daily_inventory directory

--- rpa001_daily_inventory/rpa001.bat
+++
@@ -1,2 +0,0 @@
-sqlcmd -S "SRV2-SAP" -d DBReport -U sa -P @SapN22RwDt@ -i "f:\bot\rpa001\sql_setnocount.sql","f:\bot\rpa001\rpa001.sql" -o "f:\bot\rpa001\rpa001.csv"  -s";" -W -f 65001 
-python f:\bot\rpa001\rpa001.py
